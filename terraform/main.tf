b97a1c88149e52e12ca1d8b8c6067917a769f62f
Alexander Ananchenko <snvtr@mail.ru>
2018-10-30 15:08:18 +0300

    total mess

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,3 +39,27 @@ resource "google_compute_instance" "app" {
   }
 }
 
+resource "google_compute_firewall" "firewall_puma" {
+  name     = "allow-puma-default"
+  # Название сети для которой действует правило
+  network  = "default"
+  # Какой доступ разрешаем
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+  # кому разрешаем ходить
+  source_ranges = ["0.0.0.0/0"]
+  # на какой тэг вешается правило
+  target_tags = ["reddit-app-terraform"]
+}
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name    = "default-allow-ssh"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
