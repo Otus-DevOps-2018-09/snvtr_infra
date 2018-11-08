d88a1d8dba16e4e4c9510b39916a278c070458af
Alexander Ananchenko <snvtr@mail.ru>
2018-11-08 15:48:20 +0300

    Ansible-2: #1

---
+++ terraform/modules/db/variables.tf
@@ -0,0 +1,13 @@
+variable zone {
+  description = "Region/Zone"
+  default     = "europe-west1-b"
+}
+
+variable public_key_path {
+  description = "Path to my public key"
+}
+
+variable db_disk_image {
+  description = "DB disk image"
+}
+
---
+++ terraform/prod/variables.tf
@@ -0,0 +1,25 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable zone {
+  description = "Region/Zone"
+  default     = "europe-west1-b"
+}
+
+variable public_key_path {
+  description = "Path to my public key"
+}
+
+variable app_disk_image {
+  description = "App disk image"
+}
+
+variable db_disk_image {
+  description = "DB disk image"
+}
