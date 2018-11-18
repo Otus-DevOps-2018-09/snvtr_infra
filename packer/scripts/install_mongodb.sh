5784546044f98a61497bcc49e5320e3519467cee
Alexander A <a.ananchenko@gmail.com>
2018-11-18 17:59:43 +0000

    Ansible-4: #1

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -7,6 +7,6 @@ bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 mul
 apt update
 apt install -y mongodb-org
 
-systemctl start mongod
-systemctl enable mongod
+#systemctl start mongod
+#systemctl enable mongod
 
